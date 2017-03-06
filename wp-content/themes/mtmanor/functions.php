d7ce466acea7c83a3a67dacfda8f76ab2ebd30c0
Brad Sawicki <git@bradsawicki.com>
2017-03-06 15:14:57 +0100

    Product/Subcat thumb wrapper function update

--- wp-content/themes/mtmanor/functions.php
+++ wp-content/themes/mtmanor/functions.php
@@ -248,19 +248,21 @@ add_action( 'woocommerce_after_subcategory', 'mtm_template_loop_category_link_cl
 
 
 // Loop Product Thumb Wrapper
-remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
-function woocommerce_template_loop_product_thumbnail( $category ) {
-	echo '<div class="product-grid--thumb">' . woocommerce_get_product_thumbnail() . '</div>';
-}
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+add_action( 'woocommerce_before_shop_loop_item_title', function() {
+  echo '<div class="product-grid--thumb">';
+}, 9 );
 
+add_action( 'woocommerce_before_shop_loop_item_title', function() {
+  echo '</div>';
+}, 11 );
 
-// Category Loop Product Thumb Wrapper
-remove_action( 'woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10);
-function woocommerce_subcategory_thumbnail() {
-	echo '<div class="product-grid--thumb">' . woocommerce_get_product_thumbnail() . '</div>';
-}
-add_action( 'woocommerce_before_subcategory_title', 'woocommerce_subcategory_thumbnail', 10);
+add_action( 'woocommerce_before_subcategory_title', function() {
+  echo '<div class="product-grid--thumb">';
+}, 9 );
+
+add_action( 'woocommerce_before_subcategory_title', function() {
+  echo '</div>';
+}, 11 );
 
 
 // Update Loop Product Title

7d49b96a68ffac7c530cfead1ef3c68530cf980a
Brad Sawicki <git@bradsawicki.com>
2017-06-05 21:38:43 +0200

    Checkout button wc tpl fix / Favicon

--- wp-content/themes/mtmanor/header.php
+++ wp-content/themes/mtmanor/header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=1.0" name="viewport">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.svg" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>

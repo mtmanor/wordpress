6c0275058a66266bfcdcccd2d1dd116edcd38dbc
Brad Sawicki <git@bradsawicki.com>
2017-06-13 15:17:19 +0200

    Remove WP emoji junk code

--- wp-content/themes/mtmanor/functions.php
+++ wp-content/themes/mtmanor/functions.php
@@ -10,6 +10,23 @@ function mtm_setup() {
 	add_theme_support( 'wc-product-gallery-slider' );
 }
 
+function disable_wp_emojicons() {
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+
+	add_filter( 'emoji_svg_url', '__return_false' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+
 add_action( 'wp_enqueue_scripts', 'mtm_load_scripts' );
 function mtm_load_scripts() {
 

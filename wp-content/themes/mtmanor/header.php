fe2aef628641f27ec124f1722a48b7d95aff53e1
Brad Sawicki <git@bradsawicki.com>
2017-09-15 11:03:28 +0200

    Google site verif meta

--- wp-content/themes/mtmanor/header.php
+++ wp-content/themes/mtmanor/header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta content="width=device-width, initial-scale=1.0, user-scalable=1, minimum-scale=1.0" name="viewport">
+	<meta name="google-site-verification" content="RE8rEXRngLwBj5agGd1T2hD1iv0R9CdfA0i-A7TAus0" />
 	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/images/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon-16x16.png">

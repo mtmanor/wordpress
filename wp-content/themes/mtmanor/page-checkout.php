ef26387df11acae584daeb73931a41c3cb0f4b36
Brad Sawicki <git@bradsawicki.com>
2017-10-25 11:23:37 +0200

    Klarna checkout tpl/styles / order summary

--- wp-content/themes/mtmanor/page-checkout.php
+++ wp-content/themes/mtmanor/page-checkout.php
@@ -10,8 +10,13 @@
 		<section class="cart">
 			<div class="container">
 
-				<div class="checkout-forms">
-					<?php the_content(); ?>
+				<div class="checkout flex-grid">
+					<div class="checkout-form col-2-3">
+						<?php the_content(); ?>
+					</div>
+					<div class="checkout-summary col-1-3">
+						<?php echo do_shortcode( '[woocommerce_klarna_checkout_widget hide_columns="remove,price" order_note="hide"]' ); ?>
+					</div>
 				</div>
 
 			</div>
